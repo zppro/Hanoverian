957811a0dfeb910f38121212ea31baae0a3e1ed4
zppro <zpprozppro@gmail.com>
2017-07-12 16:59:19 +0800

    0.0.3 make cube-brick && yargs

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * index Created by zppro on 17-7-12.
+ * Target:入口
+ */
+import path from 'path';
+import fs from 'fs-extra';
+import brick, { logger } from 'cube-brick';
+import yargs from 'yargs'
+logger.d('yargs', yargs.argv, '------------------');
